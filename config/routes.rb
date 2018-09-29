0e0d9f483256f0857b007aa270b8b3966e19903e
colleamina <colleamina@hotmail.fr>
2018-09-29 10:08:16 -0400

    order

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
+
 Rails.application.routes.draw do
   resources :pizzas
+  post 'orders/creer_commande', to: 'orders#creer_commande'
+  get 'orders/liste_commande', to: 'orders#liste_commande'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
